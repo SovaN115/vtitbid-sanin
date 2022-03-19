0b1bab773a28d1b9f7fd24351097c0990958905c
SovaN115 <TIMOFY.SANIA@GMAIL.COM>
2022-03-19 13:00:00 +0300

    19.03.22

---
+++ Vtitbid.Sanin.Console/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Vtitbid.Sanin.Console.Models
+{
+    public class Student : Person
+    {
+        public string Group { get; set; }
+        public Student(string FirstName, string Lastname, string group)
+            : base(FirstName, Lastname) 
+        {
+            Group = group;
+        }
+
+        public Student(Person person, string group) 
+            : base(person.LastName, person.FirstName)
+        {
+            Group = group;
+        }
+    }
+
+
+}
---
+++ Vtitbid.Sanin.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Vtitbid.Sanin.Console.Models;
+
+Console.WriteLine("Hello, World!");
+
+Person timofy = new Person("Timofy", "Sanin");
+Console.WriteLine(timofy.FirstName);
+
